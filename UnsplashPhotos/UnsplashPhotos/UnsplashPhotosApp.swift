d499bb7f752ee82f4e9d14cc16a778fad4eeb9b9
Mac <vachhani.vibha95@gmail.com>
2024-08-02 18:50:00 -0400

    Initial Project Creation

---
+++ UnsplashPhotos/UnsplashPhotos/UnsplashPhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashPhotosApp.swift
+//  UnsplashPhotos
+//
+//  Created by Mac on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashPhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
